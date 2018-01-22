e39b2237baf688b4bbe9846697a6d7785ac278db
edgar <edgrr.trem000@gmail.com>
2018-01-21 19:44:30 -0600

    Include Google APi Client, create Auto Complete prediction list

---
+++ googleplacessearchdialog/src/main/java/com/taskail/googleplacessearchdialog/GoogleAutoCompletePredictions.kt
@@ -0,0 +1,59 @@
+package com.taskail.googleplacessearchdialog
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.data.DataBufferUtils
+import com.google.android.gms.location.places.AutocompleteFilter
+import com.google.android.gms.location.places.AutocompletePrediction
+import com.google.android.gms.location.places.Places
+import com.google.android.gms.maps.model.LatLngBounds
+import java.util.ArrayList
+import java.util.concurrent.TimeUnit
+
+/**
+ *Created by ed on 1/21/18.
+ */
+
+class GoogleAutoCompletePredictions(
+        private val googleApiClient: GoogleApiClient,
+        private val latLngBounds: LatLngBounds,
+        private val placeFilter: AutocompleteFilter,
+        private val context: Context) {
+
+    fun getAutocompletePredictions(constraint: CharSequence): ArrayList<AutocompletePrediction>? {
+        if (googleApiClient.isConnected) {
+            Log.i("DialogAdapter", "Starting autocomplete query for: " + constraint)
+
+            // Submit the query to the autocomplete API and retrieve a PendingResult that will
+            // contain the results when the query completes.
+            val results = Places.GeoDataApi
+                    .getAutocompletePredictions(googleApiClient, constraint.toString(),
+                            latLngBounds, placeFilter)
+
+            // This method should have been called off the main UI thread. Block and wait for at most 60s
+            // for a result from the API.
+            val autocompletePredictions = results
+                    .await(60, TimeUnit.SECONDS)
+
+            // Confirm that the query completed successfully, otherwise return null
+            val status = autocompletePredictions.status
+            if (!status.isSuccess) {
+                Toast.makeText(context, "Error contacting API: " + status.toString(),
+                        Toast.LENGTH_SHORT).show()
+                Log.e("DialogAdapter", "Error getting autocomplete prediction API call: " + status.toString())
+                autocompletePredictions.release()
+                return null
+            }
+
+            Log.i("DialogAdapter", "Query completed. Received " + autocompletePredictions.count
+                    + " predictions.")
+
+            // Freeze the results immutable representation that can be stored safely.
+            return DataBufferUtils.freezeAndClose(autocompletePredictions)
+        }
+        Log.e("DialogAdapter", "Google API client is not connected for autocomplete query.")
+        return null
+    }
+}
--- googleplacessearchdialog/src/main/java/com/taskail/googleplacessearchdialog/SimpleSearchFilter.kt
+++ googleplacessearchdialog/src/main/java/com/taskail/googleplacessearchdialog/SimpleSearchFilter.kt
@@ -2,14 +2,27 @@ package com.taskail.googleplacessearchdialog
 
 import android.widget.Filter
 import com.google.android.gms.location.places.AutocompletePrediction
+import java.util.ArrayList
 
 /**
  *Created by ed on 1/21/18.
  */
-class SimpleSearchFilter : Filter() {
+class SimpleSearchFilter(private var resultList: ArrayList<AutocompletePrediction>,
+                         private val placesPrediction: GoogleAutoCompletePredictions) : Filter() {
 
     override fun performFiltering(p0: CharSequence?): FilterResults {
         val results = FilterResults()
+
+        if (p0 != null) {
+            // Query the autocomplete API for the (constraint) search string.
+
+            if (placesPrediction.getAutocompletePredictions(p0) != null) {
+                resultList = placesPrediction.getAutocompletePredictions(p0)!!
+                // The API successfully returned results.
+                results.values = resultList
+                results.count = resultList.size
+            }
+        }
 
         return results
     }
